375566284a31073d846f90fa1f670cad426acb7a
Мария <masha2355@gmail.com>
2023-07-18 21:19:39 +1000

    JoCoCo100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
-
+    
     @Test
-    void findMax() {
+    void findMaxOfMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 14, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 14;
 
         long actual = service.findMax(incomesInBillions);
 
